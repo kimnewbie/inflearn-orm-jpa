b0ec90fa2fe127c804966f11b0b769e0a1698777
kimnewbie <gracecancho@gmail.com>
2024-12-28 23:17:56 +0900

    feat: 필드와 컬럼 매핑

--- ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
@@ -1,8 +1,10 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 // JPA가 관리하는 클래스 @Entity
 @Entity
@@ -11,22 +13,59 @@ public class Member {
 
     @Id
     private Long id;
-    private String name;
+
+    // DB 컬럼명이 name
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    private Integer age;
+
+    // @Enumerated = enum 타입 매핑
+    // ** EnumType.ORDINAL: enum 순서를 데이터베이스에 저장 -> 사용X **
+    // EnumType.STRING: enum 이름을 데이터베이스에 저장
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    // 구 버전
+    // @Temporal = 날짜 타입 매핑
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    // 최신 버전
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    // @Lob = BLOB, CLOB 매핑
+    // String -> CLOB 나머지 BLOB
+    @Lob
+    private String description;
+
+    // 특정 필드를 컬럼에 매핑하지 않음(매핑 무시)
+    @Transient
+    private int temp;
 
     public Member() {
     }
 
-    public Member(Long id, String name) {
+    public Member(Long id, String username, Integer age, RoleType roleType, Date createdDate, Date lastModifiedDate, String description) {
         this.id = id;
-        this.name = name;
+        this.username = username;
+        this.age = age;
+        this.roleType = roleType;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.description = description;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String username) {
+        this.username = username;
     }
 
     public Long getId() {
@@ -34,6 +73,6 @@ public class Member {
     }
 
     public String getName() {
-        return name;
+        return username;
     }
 }
