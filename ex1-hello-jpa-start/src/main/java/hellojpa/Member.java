27445c44330d4b8434f2f441a3d3d235c0aeca95
kimnewbie <gracecancho@gmail.com>
2024-12-28 22:51:34 +0900

    comment: 객체와 테이블 매핑

--- ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
@@ -2,8 +2,11 @@ package hellojpa;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+// JPA가 관리하는 클래스 @Entity
 @Entity
+//@Table(name="MBR") // 매핑할 때 사용됨. 실제 db 명
 public class Member {
 
     @Id
