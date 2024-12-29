af522aafbdaa9d2672d425f87e6c2d8577f87f74
kimnewbie <gracecancho@gmail.com>
2024-12-29 19:02:35 +0900

    refactor: 객체지향 연관관계 매핑 기초

--- ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
@@ -8,19 +8,31 @@ import java.util.Date;
 
 // JPA가 관리하는 클래스 @Entity
 @Entity
-//@Table(name="MBR") // 매핑할 때 사용됨. 실제 db 명
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 50)
 public class Member {
 
-    @Id // 직접 할당
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR") // 자동 할당
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false)
-    private String username;
+    @Column(name = "USERNAME")
+    private String name;
+
+    private int age;
+
+    @Column(name = "TEAM_ID")
+    private Long teamId;
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+//    @ManyToOne
+//    @JoinColumn(name = "TEAM_ID")
+//    private Team team;
 
     public Member() {
     }
@@ -29,15 +41,23 @@ public class Member {
         return id;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
